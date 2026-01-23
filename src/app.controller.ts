35aa82e34198702891a6262993c91b72b20ae1a0
Pedro Lyrio <pedrolyrio@ucl.br>
2026-01-23 10:38:21 -0300

    feat: implement health check endpoint in AppController and update AppService for health status response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProduces } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiProduces('application/json')
+  @ApiOkResponse({
+    description: 'Health check',
+    schema: {
+      example: {
+        status: 'ok',
+        service: 'ShieldAuth API',
+        timestamp: '2026-01-23T13:29:20.332Z',
+      },
+    },
+  })
+  health() {
+    return this.appService.healthCheck();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  healthCheck() {
+    return {
+      status: 'ok',
+      service: 'ShieldAuth API',
+      timestamp: new Date().toDateString(),
+    };
   }
 }
